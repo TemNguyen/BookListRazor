cef3566688d087c69d92abca09f55b8cbf0ae7fd
Nguyễn Duy Thịnh <temthinh30012001@gmail.com>
2021-09-13 21:53:51 +0700

    HttpGET api

---
+++ BookViewRazor/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using BookViewRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookViewRazor.Controllers
+{
+    [Route("api/Books")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public IActionResult GetAllBook()
+        {
+            return Json(new
+            {
+                data = _db.Books.ToList()
+            });
+        }
+    }
+}
